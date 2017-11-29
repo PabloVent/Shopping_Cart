ba5f55130636fed187f959de84da9ec8c0434629
pablo_vent <pvent08@gmail.com>
2017-11-29 23:10:19 +0000

    twick to display products on the index web page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+  var products = Product.find();
+  res.render('shop/index', { title: 'Shopping Cart', products: products });
 });
 
 module.exports = router;
