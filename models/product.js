83dc87d3cbdf8e208b2eef5dc9db9a46d01c4f7d
pablo_vent <pvent08@gmail.com>
2017-11-29 22:22:09 +0000

    feeding DB with dummy products

--- models/product.js
+++ models/product.js
@@ -1,2 +1,11 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    imagePath: {type:string, required:true},
+    title: {type:string, required:true},
+    description: {type:string, required:true},
+    price: {type:Number, required:true}
+})
+
+model.exports = mongoose.model('Product', schema); // To use blueprint in my models.
---
+++ seed/product-seeder.js
@@ -0,0 +1,44 @@
+var Product = require('../models/product');
+
+var products = [
+    new Product({
+    imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWcHeSExjUYaYOh_l5mo7-5ALLTusK6QoNaVgmsmuuF1Tz64K8',
+    title: 'Freiburg',
+    description: 'awesome town!!!!!',
+    price: 120
+}),
+new Product({
+    imagePath: 'https://a.thumbs.redditmedia.com/41Jhg1UypfSljaq0JYfpITlAqlQNe1BwLn6SMwHCzX0.png',
+    title: 'Overwatch',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
+    price: 10
+}),
+
+new Product({
+    imagePath: 'http://i1-news.softpedia-static.com/images/news2/life-is-strange-review-pc-499548-2.jpg',
+    title: 'Life is Strange',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
+    price: 10
+}),
+
+new Product({
+    imagePath: 'http://ih0.redbubble.net/image.198189443.1386/pp,220x200-pad,220x200,ffffff.jpg',
+    title: 'Fire Emblem Fates: Revelation',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
+    price: 10
+}),
+
+new Product({
+    imagePath: 'http://fontmeme.com/images/undertale-font.jpg',
+    title: 'Undertale',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
+    price: 10
+}),
+
+new Product({
+    imagePath: 'http://static3.gamespot.com/uploads/scale_tiny/mig/4/6/3/2/2214632-box_ff12.png',
+    title: 'Final Fantasy XII',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
+    price: 10
+})
+]
