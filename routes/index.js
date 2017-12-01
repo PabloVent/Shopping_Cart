63eecf3ca6bebfa86c2b6a566bd424e08af2016a
pablo_vent <pvent08@gmail.com>
2017-12-01 16:03:21 +0000

    working on entry point to loop thru chuncks of 3 products, and then loop through each chunck

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs){
+    var productChuncks = [];
+    var chunckSize = 3;
     res.render('shop/index', { title: 'Shopping Cart', products: docs });
   });
   
