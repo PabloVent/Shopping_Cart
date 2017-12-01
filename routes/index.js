069015f513b901e39fe1ec6f06789880eca16a13
pablo_vent <pvent08@gmail.com>
2017-12-01 17:36:34 +0000

    fixed display issue and reload DB with new data

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,10 @@ router.get('/', function(req, res, next) {
   Product.find(function(err, docs){
     var productChuncks = [];
     var chunckSize = 3;
-    res.render('shop/index', { title: 'Shopping Cart', products: docs });
+    for (let i = 0; i < docs.length; i+= chunckSize) {
+      productChuncks.push(docs.slice(i, i + chunckSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChuncks });
   });
   
 });
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -5,46 +5,37 @@ mongoose.connect('localhost:27017/shopping');
 
 var products = [
     new Product({
-    imagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWcHeSExjUYaYOh_l5mo7-5ALLTusK6QoNaVgmsmuuF1Tz64K8',
-    title: 'Freiburg',
-    description: 'awesome town!!!!!',
-    price: 120
-}),
-new Product({
-    imagePath: 'https://a.thumbs.redditmedia.com/41Jhg1UypfSljaq0JYfpITlAqlQNe1BwLn6SMwHCzX0.png',
-    title: 'Overwatch',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
-    price: 160
-}),
+        imagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+        title: 'Gothic Video Game',
+        description: 'Awesome Game!!!!',
+        price: 10
+    }),
+    new Product({
+        imagePath: 'http://eu.blizzard.com/static/_images/games/wow/wallpapers/wall2/wall2-1440x900.jpg',
+        title: 'World of Warcraft Video Game',
+        description: 'Also awesome? But of course it was better in vanilla ...',
+        price: 20
+    }),
+    new Product({
+        imagePath: 'https://support.activision.com/servlet/servlet.FileDownload?file=00PU000000Rq6tz',
+        title: 'Call of Duty Video Game',
+        description: 'Meh ... nah, it\'s okay I guess',
+        price: 40
+    }),
+    new Product({
+        imagePath: 'https://pmcdeadline2.files.wordpress.com/2014/02/minecraft__140227211000.jpg',
+        title: 'Minecraft Video Game',
+        description: 'Now that is super awesome!',
+        price: 15
+    }),
+    new Product({
+        imagePath: 'https://d1r7xvmnymv7kg.cloudfront.net/sites_products/darksouls3/assets/img/DARKSOUL_facebook_mini.jpg',
+        title: 'Dark Souls 3 Video Game',
+        description: 'I died!',
+        price: 50
+    })
+];
 
-new Product({
-    imagePath: 'http://i1-news.softpedia-static.com/images/news2/life-is-strange-review-pc-499548-2.jpg',
-    title: 'Life is Strange',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
-    price: 109
-}),
-
-new Product({
-    imagePath: 'http://ih0.redbubble.net/image.198189443.1386/pp,220x200-pad,220x200,ffffff.jpg',
-    title: 'Fire Emblem Fates: Revelation',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
-    price: 119
-}),
-
-new Product({
-    imagePath: 'http://fontmeme.com/images/undertale-font.jpg',
-    title: 'Undertale',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
-    price: 310
-}),
-
-new Product({
-    imagePath: 'http://static3.gamespot.com/uploads/scale_tiny/mig/4/6/3/2/2214632-box_ff12.png',
-    title: 'Final Fantasy XII',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet nostrum nesciunt, quidem doloremque numquam temporibus ut optio ipsam! Reprehenderit autem impedit dolore eos doloribus sapiente illo, perferendis assumenda reiciendis, et?',
-    price: 1000
-})
-]
 var done = 0;
 for (let i = 0; i < products.length; i++) {
     products[i].save(function(err, result){
