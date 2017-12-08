a7fcf0180a5bbbb96a832168be34a9c55bbdb9d4
pablo_vent <pvent08@gmail.com>
2017-12-08 23:18:03 +0000

    struggling on with fixing the bug

--- config/passport.js
+++ config/passport.js
@@ -2,11 +2,9 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-// passport.serializeUser(function(user, done){
-//     done(null, user.id); // serialize user by id whenever you want to store it in your session.
-// }); // Tells passport how to store the user in the session.
-
-passport.serializeUser(function(user, done){ done(null, user._id); // serialize user by id whenever you want to store it in your session. });
+passport.serializeUser(function(user, done){
+    done(null, user.id); // serialize user by id whenever you want to store it in your session.
+}); // Tells passport how to store the user in the session.
 
 passport.deserializeUser(function(id, done){
     User.findById(id, function(err, user){ // in mongoDB using mongoose; id is stored in the session.
@@ -39,4 +37,4 @@ passport.use('local.signup', new LocalStrategy({
         })
     });
 }));
-})
+
