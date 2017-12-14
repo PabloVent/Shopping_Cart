ec7e79b566923e3c3276c63e1afe6b5cebf58c58
Pablo Vent <Ventp.work@gmail.com>
2017-12-14 23:53:02 +0000

    formatting code

--- config/passport.js
+++ config/passport.js
@@ -2,12 +2,12 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done) {
     done(null, user.id); // serialize user by id whenever you want to store it in your session.
 }); // Tells passport how to store the user in the session.
 
-passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){ // in mongoDB using mongoose; id is stored in the session.
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) { // in mongoDB using mongoose; id is stored in the session.
         done(err, user);
     })
 })
@@ -18,23 +18,26 @@ passport.use('local.signup', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-}, function(req, email, password, done) {
-    User.findOne({'email': email}, function(err, user){
-        if(err){
+}, function (req, email, password, done) {
+    User.findOne({
+        'email': email
+    }, function (err, user) {
+        if (err) {
             return done(err);
         }
-        if(user){
-            return(done(null, false, {message: 'Email is already in use'})); // No errors(null), but search not successful (false); password already in use.
+        if (user) {
+            return (done(null, false, {
+                message: 'Email is already in use'
+            })); // No errors(null), but search not successful (false); password already in use.
         }
         var newUser = new User();
         newUser.email = email;
         newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err, result){
-            if(err){
+        newUser.save(function (err, result) {
+            if (err) {
                 return done(err);
             }
             return done(null, newUser);
         })
     });
 }));
-
